1d8f0f505c4578226030c6cb2315a163ae5a1e6f
DIOR <diertojiev@yandex.ru>
2024-12-17 11:33:49 +0300

    An attempt to fix an error with receiving an image via the API

--- src/services/CoffeeApi.js
+++ src/services/CoffeeApi.js
@@ -1,50 +1,51 @@
 class CoffeApi {
-    _apiBase = "https://fake-coffee-api.vercel.app/api";
+	_apiBase = "https://fake-coffee-api.vercel.app/api";
 
-    async getResource(url = this._apiBase) {
-        let res = await fetch(url);
+	async getResource(url = this._apiBase) {
+		let res = await fetch(url);
 
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
 
-        return await res.json();
-    }
+		return await res.json();
+	}
 
-    async getAllCoffee() {
-        let res = await this.getResource();
+	async getAllCoffee() {
+		let res = await this.getResource();
 
-        return await res.map(this._transformCoffee);
-    }
+		return await res.map(this._transformCoffee);
+	}
 
-    async getCoffeeById(id, url = this._apiBase) {
-        let res = await fetch(`${url}/${id}`);
+	async getCoffeeById(id, url = this._apiBase) {
+		let res = await fetch(`${url}/${id}`);
 
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
 
-        return await res.json();
-    }
+		return await res.json();
+	}
 
-    _transformCoffee(coffee) {
-        return {
-            id: coffee.id,
-            name: coffee.name,
-            descr: coffee.description,
-            price: coffee.price,
-            region: coffee.region,
-            img: this._getImageUrl(coffee.image_url), // Прокси изображений
-            roast_level: coffee.roast_level,
-            flavor_profile: coffee.flavor_profile,
-            grind_option: coffee.grind_option,
-        };
-    }
+	async _transformCoffee(coffee) {
+		console.log(coffee.image_url);
+		return {
+			id: coffee.id,
+			name: coffee.name,
+			descr: coffee.description,
+			price: coffee.price,
+			region: coffee.region,
+			img: await this._getImageUrl(coffee.image_url), // Прокси изображений
+			roast_level: coffee.roast_level,
+			flavor_profile: coffee.flavor_profile,
+			grind_option: coffee.grind_option,
+		};
+	}
 
-    // Функция для проксирования изображений
-    _getImageUrl(imageUrl) {
-        return `https://feather-polite-delivery.glitch.me/proxy-image?url=${encodeURIComponent(imageUrl)}`;
-    }
+	// Функция для проксирования изображений
+	async _getImageUrl(imageUrl) {
+		return `https://feather-polite-delivery.glitch.me/proxy-image?url=${imageUrl}`;
+	}
 }
 
 export default CoffeApi;
